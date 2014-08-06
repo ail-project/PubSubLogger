e54137dd8caab4d5003a14b49fe76e58d4a3696e
Raphaël Vinot <raphael@vinot.info>
2014-08-06 11:21:23 +0200

    Autorotate the files each days.
    Cleanup and update version number.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='pubsublogger',
-    version='1.0',
+    version='1.1',
     author='Raphaël Vinot',
-    author_email='raphael.vinot@gmail.com',
+    author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
     url='https://github.com/Rafiot/PubSubLogger',
     description='Logging system using the PubSub functionality of Redis.',
     packages=['pubsublogger'],
-    scripts = ['log_subscriber'],
+    scripts=['log_subscriber'],
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Development Status :: 5 - Production/Stable',
@@ -22,4 +22,4 @@ setup(
         'Topic :: System :: Logging'
     ],
     install_requires=['redis', 'logbook']
-    )
+)
--- test/test_publisher.py
+++ test/test_publisher.py
@@ -10,16 +10,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Configure a logging publisher.')
 
     parser.add_argument('-s', '--use_unix_socket',  action='store_true',
-            help='Use a unix socket path instead of a tcp socket.')
+                        help='Use a unix socket path instead of a tcp socket.')
     parser.add_argument("--unix_socket_path", default='/tmp/redis.sock',
-            type=str, help='Unix socket path.')
+                        type=str, help='Unix socket path.')
 
     parser.add_argument("-H", "--hostname", default='localhost',
-            type=str, help='Set the hostname of the server.')
+                        type=str, help='Set the hostname of the server.')
     parser.add_argument("-p", "--port", default=6379,
-            type=int, help='Set the server port.')
+                        type=int, help='Set the server port.')
     parser.add_argument("-c", "--channel",
-            type=str, required=True, help='Channel to publish into.')
+                        type=str, required=True, help='Channel to publish into.')
 
     args = parser.parse_args()
 
@@ -32,14 +32,14 @@ if __name__ == '__main__':
 
     publisher.channel = args.channel
 
-    for i in range(0,21):
-        if i%2 == 0 :
+    for i in range(0, 21):
+        if i % 2 == 0:
             publisher.info('test' + str(i))
-        elif i%3 == 0 :
+        elif i % 3 == 0:
             publisher.warning('test' + str(i))
-        elif i%5 == 0:
+        elif i % 5 == 0:
             publisher.error('test' + str(i))
-        elif i%7 == 0:
+        elif i % 7 == 0:
             publisher.critical('test' + str(i))
         else:
             publisher.debug('test' + str(i))
