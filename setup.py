516dd6a456f9774e58dd2780e00d0ec9303f94c0
Raphaël Vinot <raphael@vinot.info>
2015-11-03 14:36:54 +0100

    Update to 1.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pubsublogger',
-    version='1.2.1',
+    version='1.2.2',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
